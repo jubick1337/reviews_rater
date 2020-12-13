a7ab3dcd5749c8b47dcdd42f47055dbed04d652e
matvey <mattyson.so@gmail.com>
2020-12-13 22:57:58 +0500

    added text for result

--- reviews_rater/views.py
+++ reviews_rater/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+
 from .forms import MemoryModelForm
 from .model_holder import model
+
+result_mapper = {0: 'bad', 1: 'good'}
 
 
 def index(request):
@@ -8,7 +11,7 @@ def index(request):
     if form.is_valid():
         review = form.cleaned_data.get('review')
         result = model(review).item()
-        context = {'form': form, 'result': f'Review is {result}'}
+        context = {'form': form, 'result': f'Review is {result_mapper[result]}'}
         return render(request, 'index.html', context)
 
     return render(request, 'index.html', {'form': form})
